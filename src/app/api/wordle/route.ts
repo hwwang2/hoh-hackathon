ba2fbc7fb3f785e937d1206d35efd765e9ad3436
wanghongwei <1@q.com>
2025-01-23 17:49:31 +0800

    添加本地环境变量测试

--- src/app/api/wordle/route.ts
+++ src/app/api/wordle/route.ts
@@ -1,14 +1,17 @@
+import { type NextRequest } from 'next/server'
+
 export const dynamic = 'force-static'
  
-export async function GET() {
+export async function GET(request: NextRequest) {
 //   const res = await fetch('https://data.mongodb-api.com/111', {
 //     headers: {
 //       'Content-Type': 'application/json',
 //       'API-Key': process.env.DATA_API_KEY,
 //     },
 //   })
-  const data = {"ok":1};
- 
-  return Response.json({ data })
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get('token');
+    const data = {"ok": query===process.env.ADMIN_TOKEN ? 1 : 0};
+    return Response.json({ data })
 }
 
